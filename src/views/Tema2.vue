e89a07b9a41d8576222678724599988898c3b3d4
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-06-13 21:05:12 -0500

    avanca 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,105 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  .container.container01.px-4.px-md-5.pt-4
+    .titulo-principal.color-acento-contenido(data-aos="fade-right")
       .titulo-principal__numero
-        span 2
-      h1 Titulo de primer nivel
+        span.text-white 2
+      h1 Habeas data y normatividad vigente
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-4.col-8.p-0
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema2/img01.png')
+      .col-md-8.col-12.BG01.p-3(data-aos="fade-left")    
+          p <span style="background-color:#ECE5F7 !important; font-weight: bold;">Cuando las empresas usan datos personales para aplicar técnicas y tareas de marketing deberán valorar aspectos éticos</span><span style="color:#7F4FC7 !important; font-weight: bold;">(Ronda, 2013). </span> Aquí se tiene la capacidad de ahondar y conocer los pensamientos más profundos y privados de los consumidores deberá realizarse de forma transparente y destinando la información obtenida a la creación de productos y servicios que satisfagan las necesidades y deseos reales de los consumidores. <b>Además, permite conocer las rutas y hábitos para la decisión de compra teniendo la susceptibilidad de ser manipulado y, asimismo, evitar publicar anuncios engañosos o abusivos aprovechando la vulnerabilidad o necesidad de algún grupo poblacional</b>. Es importante anotar que los procesos de información deben tener autorización por parte de los implicados.
+
+    separador
+
+    #t_2_1.titulo-segundo.color-acento-contenido.ms-5(data-aos="fade-right")
+      h2 2.1.	Manejo de la información personal - Habeas data
+
+  .container.BGIMG02.pe-5.pt-4
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-8.col-12.BG02.px-3.ps-5.mb-5.order-md-1.order-2(data-aos="fade-right")
+        .row.justify-content-center.align-items-center.mb-2
+          .col-md-1.py-4
+            figure
+              img(src='@/assets/curso/tema2/img04.svg')
+          .col-md-10
+            p El derecho de habeas data es aquel que tiene toda persona para conocer, actualizar y rectificar la información que se haya recogido sobre ella en archivos y bancos de datos de naturaleza pública o privada. (Superintendencia de Industria y Comercio, 2021).
+        p La normatividad en Colombia está sustentada en la Ley 126 de 2008, en diversas sentencias posteriores de la Corte y otras disposiciones que van actualizando la ley a medida de las necesidades.
+        .cajon.color-acento-botones.p-3
+          p Es importante tener en cuenta algunos concretos que se encuentran en la norma, según la Superintendencia de Industria y Comercio (2021), a saber:
+      .col-md-4.col-8.p-0.mb-5.order-md-2.order-1
+        figure(data-aos="fade-left")
+          img(src='@/assets/curso/tema2/img03.png')  
+      .col-md-5.col-10.p-0.ps-5.mb-5.order-3
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema2/img05.svg') 
+      .col-md-7.col-11.ps-5.order-4(data-aos="fade-left")
+        LineaTiempoD.color-acento-botones
+          .row.justify-content-center.align-items-center(numero="1" titulo="Titular de la información")
+            .col-md-11
+              p Persona natural o jurídica a quien se refiere la información que reposa en un banco de datos.
+          .row.justify-content-center.align-items-center(numero="2" titulo="Operador de información")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img06.svg') 
+            .col-md-8.col-7
+              p Persona u organización que recibe de la fuente datos personales sobre varios titulares de la información, los administra y los pone en conocimiento de los usuarios bajo los parámetros de la ley.
+          .row.justify-content-center.align-items-center(numero="3" titulo="Usuario de la información")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img07.svg') 
+            .col-md-8.col-7
+              p Persona natural o jurídica que puede acceder a información personal de uno o varios titulares de la información suministrada por el operador o por la fuente, o directamente por el titular de la información.
+          .row.justify-content-center.align-items-center(numero="4" titulo="Dato personal")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img08.svg') 
+            .col-md-8.col-7
+              p Información vinculada a una o varias personas determinadas o determinables o que puedan asociarse con una persona natural o jurídica. Los datos personales pueden ser públicos, semiprivados o privados. 
+          .row.justify-content-center.align-items-center(numero="5" titulo="Principio de finalidad")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img09.svg') 
+            .col-md-8.col-7
+              p Obliga a que las actividades de recolección de datos personales obedezcan a una finalidad legal. Con fundamento en este principio, la finalidad debe ser comunicada al titular de la información previa o concomitante con el otorgamiento del titular de la autorización.
+          .row.justify-content-center.align-items-center(numero="6" titulo="Circulación restringida")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img10.svg') 
+            .col-md-8.col-7
+              p Consiste en que, a menos que la información sea pública, los datos personales no podrán ser accesibles por Internet o por otros medios de divulgación o comunicación masiva, salvo algunas salvedades definidas por la ley.
+          .row.justify-content-center.align-items-center(numero="7" titulo="Principio de seguridad")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img11.svg') 
+            .col-md-8.col-7
+              p La información contenida en los bancos de datos, así como aquella que resulte de las consultas que realicen los usuarios, se incorporen las medidas técnicas necesarias para garantizar la seguridad de los registros para evitar su adulteración, pérdida, consulta o uso no autorizado.
+          .row.justify-content-center.align-items-center(numero="8" titulo="Principio de confidencialidad")
+            .col-md-2.col-3
+              figure
+                img(src='@/assets/curso/tema2/img12.svg') 
+            .col-md-8.col-7
+              p Todas las personas naturales o jurídicas que intervengan en la administración de datos personales que no tengan carácter público, están obligadas en todo tiempo a garantizar la reserva de la información, inclusive después de finalizada su relación con alguna de las labores que comprende la administración de datos.
+
+    .row.justify-content-center.mb-5
+      .col-lg-8.col-md-10.col-12(data-aos="fade-right")
+        .row.justify-content-center.align-items-center
+          .col-md-2.col-3
+            figure
+              img(src='@/assets/curso/tema2/img13.svg') 
+          .col-md-9
+            h5 Redondeando el importante concepto de los datos digitales se tiene lo siguiente por parte del experto:
+
+  .container.container02.px-4.px-md-5.pb-4
+    figure.mb-5(data-aos="fade-right")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,11 +1,127 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.BGIMG03.p-4.p-md-5
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
-        span 3
-      h1 Titulo de primer nivel
+        span.text-white 3
+      h1 Requerimientos del cliente
+  
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-4.col-8.p-0.mt-3
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema3/img02.png')
+      .col-md-8.col-12.BG01.p-3(data-aos="fade-left")    
+        p <b>Las organizaciones centradas en el cliente deben considerar las necesidades que los productos y servicios resuelven.</b> De manera elemental, todas las personas necesitan tener algo por deseo o necesidad, de la mejor calidad, al instante y al precio correcto. <b>Los requerimientos se basan en la gestión y profundización del producto y sus atributos, la funcionalidad que representa, los costos implicados y la disponibilidad para el cliente.</b>      
+        .row.justify-content-center.align-items-center.mb-2
+          .col-md-1.py-4
+            figure
+              img(src='@/assets/curso/tema2/img04.svg')
+          .col-md-10
+            p <span style="color:#0B67D1 !important; font-weight: bold;"> La teoría de los tres cerebros de Lean (1978), </span> proviene de la neurociencia y establece que el cerebro humano se encuentra estructurado por tres factores, un cerebro reptiliano, un cerebro límbico y un cerebro neocórtex. <span style="color:#0B67D1 !important; font-weight: bold;"> De acuerdo con los fundamentos de esta teoría, cada componente del cerebro humano cumple una función, de tal forma que el cerebro reptiliano se asocia con procesos de supervivencia, el cerebro límbico con procesos emocionales y el cerebro neocórtex con procesos racionales.</span>
+      .col-md-12
+        .cajon.color-acento-botones.p-3
+          p A partir del conocimiento de cómo funciona el cerebro humano, se podrían concluir algunos requerimientos elementales de todo cliente, así, la mayoría de las campañas, en los últimos años, apuntan a estimular o llegar al cerebro emocional y reptil, pues consideran que es aquí dónde está el “interruptor de compra” (Arboleda, 2018), al fin y al cabo, las ventas son acciones que están más enfocadas en el cerebro primitivo.
+
+    .row.justify-content-center.mb-5
+      .col-md-10
+        .titulo-sexto.color-acento-botones(data-aos="fade-right")
+          h5 Figura 9
+          span Teoría de los 3 cerebros de Paul MacLean 
+      .col-md-10.col-12
+        figure
+          img(src='@/assets/curso/tema3/img03.svg')
+
+  .container.BG02.px-4.px-md-5
+    h4.mb-5 <span style="background-color:#12263F !important; font-weight: bold; color:#FFFFFF !important;">El contexto general de los requerimientos del cliente los explica el instructor en el siguiente elemento audiovisual: </span>
+
+    figure.mb-5(data-aos="fade-right")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    separador
+
+    #t_3_1.titulo-segundo.color-acento-contenido.ms-5(data-aos="fade-right")
+      h2 3.1.	Propuestas de valor y factores clave
+
+  .container.BGIMG04.p-4.p-md-5
+    .bloque-texto-g.color-secundario.p-4.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(data-aos="zoom-in")(
+        :style="{'background-image': `url(${require('@/assets/curso/tema3/img04.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4  
+        p.mb-0 Es conveniente que las estrategias de ventas estén enfocadas a lograr convencer y enamorar el público en tres aspectos (uno para cada cerebro), de los cuales se desprenden los demás elementos de las propuestas de valor ganadoras así:
+
+    .row.mb-5
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.color-secundario.p-4
+          .row.justify-content-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/tema3/img05.svg')
+          h4 Seguridad (reptiliano) 
+          p De manera natural, los animales, incluyéndonos, nos gusta tomar riesgos, esto es un factor para el éxito de marcas y productos es la confianza, seguridad y respaldo que puedan representar.
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.color-secundario.p-4
+          .row.justify-content-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/tema3/img06.svg')
+          h4 Placer (límbico)
+          p Corresponde al desarrollo de sensaciones o emociones positivas y agradables, que estimulen la generación de dopamina o sensación de felicidad (Asprilla, 2019). 
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.color-secundario.p-4.pb-5 
+          .row.justify-content-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/tema3/img07.svg')
+          h4 Utilidad (neocórtex)
+          p.mb-4 Beneficio o conveniencia de algo con los interés u objetivos de alguien. 
+
+  .container.container02.px-4.px-md-5.pb-4
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-8.col-12.p-3(data-aos="fade-right")
+        p <span style="color:#0B67D1 !important; font-weight: bold;">Si bien es importante que todos los productos y servicios del negocio estén bajo el contexto de estos tres componentes, es normal que para algunas personas haya más ponderación en uno más que otros cerebros, por ejemplo, para clientes corporativos, se debe enfocar en satisfacer requerimientos racionales, en cambio, para consumidores masivos funciona mejor los enfoques al cerebro reptiliano y límbico. </span> En este orden, conviene identificar cuál es el requerimiento cerebral predominante en cada cliente y enfatizar la propuesta de valor o presentación en la satisfacción de sus exigencias.
+      .col-md-4.col-8.p-0.mt-3
+        figure(data-aos="fade-left")
+          img(src='@/assets/curso/tema3/img09.png')
+
+    separador
+
+    #t_3_2.titulo-segundo.color-acento-contenido.ms-5(data-aos="fade-right")
+      h2 3.2.	Algunos requisitos para mejorar experiencias de los clientes
+
+    p.mb-5 Actualmente, los clientes se han vuelto más exigentes y más escasos, pues el número de ofertas disponible para todos aumenta aceleradamente cada día no solo por la facilidad de crear un negocio online, sino también por las tendencias en aumento de compras usando medios digitales.
+    h4.mb-5 <span style="background-color:#12263F !important; font-weight: bold; color:#FFFFFF !important;">Algunas claves importantes para tener en cuenta, al momento de pensar en la satisfacción del cliente, son:</span>
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-4.col-8.px-3.mb-2
+        figure(data-aos="fade-right")
+          img(src='@/assets/curso/tema3/img10.png')
+      .col-md-7.col-11.mb-2
+        SlyderA(tipo="b")
+          .row.justify-content-center.align-items-end.BGIMG05.pb-5
+            .col-md-9.col-11.mt-5
+              .cajon-b.color-primario.p-3.mb-3
+                h5 Comunicación y atención 
+                p “Los clientes antes tenían la razón, ahora tienen el poder”. Disponer de canales comunicativos fáciles de encontrar y usar se vuelven de gran valor para los clientes, a todos nos gusta ser escuchados, sentirnos acompañados, tener soporte y que respondan las preguntas de manera oportuna y de manera correcta es un aspecto que motiva a seguir usando la marca y/o recomendarla.
+          .row.justify-content-center.align-items-end.BGIMG06.pb-5
+            .col-md-9.col-11.mt-5
+              .cajon-b.color-primario.p-3.mb-3
+                h5 Servicio oportuno  
+                p Se trata de atender las necesidades y expectativas de los clientes en el momento adecuado y por el canal adecuado. Además de tener la creatividad y conocimiento de usar los productos o servicios disponibles que más se ajusten a las exigencias expuestas.
+          .row.justify-content-center.align-items-end.BGIMG07.pb-5
+            .col-md-9.col-11.mt-5
+              .cajon-b.color-primario.p-3.mb-3
+                h5 Incluir a los clientes en los productos  
+                p Ya sea para el desarrollo de un producto o si existe la posibilidad de ofrecer artículos que el cliente lo pueda personalizar, es necesario incluirlo en la promoción de la oferta poniendo sus opiniones en los medios de la empresa, esto hace sentir de maravilla a estos clientes, pues ser protagonistas y sentirse importantes llena el cerebro de químicos positivos. 
+          .row.justify-content-center.align-items-end.BGIMG08.pb-5
+            .col-md-9.col-11.mt-5
+              .cajon-b.color-primario.p-3.mb-3
+                h5 Realimentar el servicio  
+                p Se debe procurar que los clientes realimenten después de la compra para saber más sobre expectativas y cumplimiento de requerimientos. Podrían ser comentarios en redes o privados, calificaciones del producto, encuestas, espacios para PQR.
+
+
 
 </template>
 
