0cebae2948dc386d44e75b15d065aaebd69d769d
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-06-15 21:42:46 -0500

    Avance 5

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -18,11 +18,53 @@
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
   
-  .container.BGIMG01
+  .container.BGIMG01.p-4.p-md-5
     separador
 
     #t_1_1.titulo-segundo.color-acento-contenido.ms-5(data-aos="fade-right")
       h2 1.1.	Herramientas y utilidades
+
+    a(@click="mostrarColaps()")  
+    #MapaGroup01(data-aos="zoom-in")
+      .row.justify-content-center.mb-5
+        .col-md-8.order-md-1.order-2
+          .row
+            .col-1
+              img.h-75(src='@/assets/curso/tema2/img04.svg')
+            .col-11
+              p Para que la comunicación y proyección comercial sean efectivas, es importante que el negocio tenga presencia en las tendencias de comportamientos sociales y de consumo; en el caso del entorno digital, existen varias opciones donde se puede establecer presencia de marca.
+          .row
+            .col-6.mb-3(v-for="num of 2")
+              button.ClaseBtn.collapsed(
+                :id="`boton${num}`"
+                data-bs-toggle="collapse" 
+                :data-bs-target="`#collapse${num < 10 ? '0'+num : num}, #collapse${num < 10 ? '0'+num : num}s`" 
+                aria-expanded="true" 
+                :aria-controls="`#collapse${num < 10 ? '0'+num : num}, #collapse${num < 10 ? '0'+num : num}s`"
+              )
+                img.img-a(:src="require(`@/assets/curso/tema1/imagen0${num}.svg`)")
+              img.img-a.collapse(
+                :id="`collapse${num < 10 ? '0'+num : num}s`"
+                src="@/assets/curso/tema1/sdw.svg" 
+                data-bs-parent="#MapaGroup01"
+                :class="{show: num == hash}"
+              )
+        .col-md-3.col-6.order-md-2.order-1
+          img(src='@/assets/curso/tema1/imagen03.png')
+      #collapse01.collapse(:class="{show: hash ? hash == 1 : false}" data-bs-parent='#MapaGroup01')
+        .row
+          .col-auto.BG08.mb-4
+            .h4.text-white Redes sociales (Meta Business)
+        p Antes de pasar a la descripción de Meta como gran horizonte de las redes sociales, es conveniente recordar algo del contexto en el siguiente video: 
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+      #collapse02.collapse(:class="{show: hash == 2}" data-bs-parent='#MapaGroup01')
+        p La naturaleza digital tiene un contexto global, es decir, el alcance y la presencia en el mundo digital están disponibles desde cualquier país y momento. Parte de las características de las redes sociales son:
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 
@@ -36,15 +78,41 @@ export default {
   name: 'Tema1',
   components: {},
   data: () => ({
-    // variables de vue
+    hash: 0,
   }),
+  watch: {
+    $route(to) {
+      if (to.hash?.length) {
+        const numero = to.hash.split('_')[1]
+        this.mostrarCollapse(numero)
+        document.getElementById('MapaGroup01').scrollIntoView({
+          behavior: 'smooth',
+        })
+      }
+    },
+  },
   mounted() {
     this.$nextTick(() => {
+      if (this.$route.hash.length) {
+        const numero = this.$route.hash.split('_')[1]
+        this.hash = numero
+        setTimeout(() => {
+          document.getElementById('MapaGroup01').scrollIntoView({
+            behavior: 'smooth',
+          })
+        }, 500)
+      }
       this.$aosRefresh()
     })
   },
   updated() {
     this.$aosRefresh()
+  },
+  methods: {
+    mostrarCollapse(id) {
+      console.log(document.querySelector('#boton' + id))
+      document.querySelector('#boton' + id).click()
+    },
   },
 }
 </script>
